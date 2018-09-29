a1c1e00bbd2edbb30a83e59a54c1c9dcc38368b0
Craig Hesling <craig@hesling.com>
2018-09-29 16:03:48 -0400

    Added documentation to AtomicContext interface

--- interfaces.go
+++ interfaces.go
@@ -1,5 +1,9 @@
 package safetyfast
 
+// AtomicContext is the interface provided by a synchronization primitive
+// that is capable of running a functions in an atomic context.
 type AtomicContext interface {
+	// Atomic will execute commiter exactly once per call in a manor that
+	// appears to be atomic with respect to other commiters.
 	Atomic(commiter func())
 }
