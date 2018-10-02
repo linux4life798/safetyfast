1e68d3dfe2a62b6170de00af622fe2eafb9534c2
Craig Hesling <craig@hesling.com>
2018-10-02 02:17:25 -0400

    More test cases

--- atomiccontext_test.go
+++ atomiccontext_test.go
@@ -101,110 +101,75 @@ func TestLockedContextHLE(t *testing.T) {
 	runtime.GOMAXPROCS(oldmaxprocs)
 }
 
-func TestRTMContextDefault(t *testing.T) {
-	const numConcurGoRoutines = 8
-	const arrayLength = 100
-	const numIterations = 5000000
-
-	if !cpuid.HasExtendedFeature(cpuid.RTM) {
-		// Let's not fail for Travis-CI
-		fmt.Println("The CPU does not support Intel RTM - Skipping RTM Test!")
-		t.Log("The CPU does not support Intel RTM - Skipping RTM Test!")
-		// t.Fatal("The CPU does not support Intel RTM")
-		return
-	}
-
-	oldmaxprocs := runtime.GOMAXPROCS(numConcurGoRoutines)
-
-	var wg sync.WaitGroup
-	var arr = make([]int, arrayLength)
-	var c = NewRTMContexDefault()
-
-	routine := func() {
-		randsrc := rand.NewSource(int64(time.Now().Second()))
-		r := rand.New(randsrc)
-
-		for i := 0; i < numIterations; i++ {
-			index := r.Int() % len(arr)
-			c.Atomic(func() {
-				arr[index]++
-			})
-		}
-		wg.Done()
-	}
-
-	wg.Add(numConcurGoRoutines)
-	for i := 0; i < numConcurGoRoutines; i++ {
-		go routine()
-	}
-	wg.Wait()
-
-	var sum int
-	for _, v := range arr {
-		sum += v
-	}
-
-	expected := numIterations * numConcurGoRoutines
-	t.Logf("Array Length=%d | NumberGoRoutines=%d | NumIterations=%d", len(arr), numConcurGoRoutines, numIterations)
-	t.Logf("ArraySum=%d | Expected=%d", sum, expected)
-	t.Logf("CapacityAborts=%d", c.CapacityAborts())
-	if sum != expected {
-		t.Fatalf("Sum result is %d, but we expected %d", sum, expected)
-	}
-
-	runtime.GOMAXPROCS(oldmaxprocs)
-}
-
 func TestRTMContext(t *testing.T) {
-	const numConcurGoRoutines = 8
-	const arrayLength = 100
-	const numIterations = 5000000
+	run := func(t *testing.T, lock sync.Locker) {
+		const numConcurGoRoutines = 8
+		const arrayLength = 100
+		const numIterations = 5000000
 
-	if !cpuid.HasExtendedFeature(cpuid.RTM) {
-		// Let's not fail for Travis-CI
-		fmt.Println("The CPU does not support Intel RTM - Skipping RTM Test!")
-		t.Log("The CPU does not support Intel RTM - Skipping RTM Test!")
-		// t.Fatal("The CPU does not support Intel RTM")
-		return
-	}
-
-	oldmaxprocs := runtime.GOMAXPROCS(numConcurGoRoutines)
-
-	var wg sync.WaitGroup
-	var arr = make([]int, arrayLength)
-	var c = NewRTMContex(new(sync.Mutex))
-
-	routine := func() {
-		randsrc := rand.NewSource(int64(time.Now().Second()))
-		r := rand.New(randsrc)
-
-		for i := 0; i < numIterations; i++ {
-			index := r.Int() % len(arr)
-			c.Atomic(func() {
-				arr[index]++
-			})
+		if !cpuid.HasExtendedFeature(cpuid.RTM) {
+			// Let's not fail for Travis-CI
+			fmt.Println("The CPU does not support Intel RTM - Skipping RTM Test!")
+			t.Log("The CPU does not support Intel RTM - Skipping RTM Test!")
+			// t.Fatal("The CPU does not support Intel RTM")
+			return
 		}
-		wg.Done()
+
+		oldmaxprocs := runtime.GOMAXPROCS(numConcurGoRoutines)
+
+		var wg sync.WaitGroup
+		var arr = make([]int, arrayLength)
+		var c *RTMContext
+		if lock == nil {
+			c = NewRTMContexDefault()
+		} else {
+			c = NewRTMContex(lock)
+		}
+
+		routine := func() {
+			randsrc := rand.NewSource(int64(time.Now().Second()))
+			r := rand.New(randsrc)
+
+			for i := 0; i < numIterations; i++ {
+				index := r.Int() % len(arr)
+				c.Atomic(func() {
+					arr[index]++
+				})
+			}
+			wg.Done()
+		}
+
+		wg.Add(numConcurGoRoutines)
+		for i := 0; i < numConcurGoRoutines; i++ {
+			go routine()
+		}
+		wg.Wait()
+
+		var sum int
+		for _, v := range arr {
+			sum += v
+		}
+
+		expected := numIterations * numConcurGoRoutines
+		t.Logf("Array Length=%d | NumberGoRoutines=%d | NumIterations=%d", len(arr), numConcurGoRoutines, numIterations)
+		t.Logf("ArraySum=%d | Expected=%d", sum, expected)
+		t.Logf("CapacityAborts=%d", c.CapacityAborts())
+		if sum != expected {
+			t.Fatalf("Sum result is %d, but we expected %d", sum, expected)
+		}
+
+		runtime.GOMAXPROCS(oldmaxprocs)
 	}
 
-	wg.Add(numConcurGoRoutines)
-	for i := 0; i < numConcurGoRoutines; i++ {
-		go routine()
-	}
-	wg.Wait()
+	t.Run("Default", func(t *testing.T) {
+		run(t, nil)
+	})
 
-	var sum int
-	for _, v := range arr {
-		sum += v
-	}
+	t.Run("sync.Mutex", func(t *testing.T) {
+		run(t, new(sync.Mutex))
+	})
 
-	expected := numIterations * numConcurGoRoutines
-	t.Logf("Array Length=%d | NumberGoRoutines=%d | NumIterations=%d", len(arr), numConcurGoRoutines, numIterations)
-	t.Logf("ArraySum=%d | Expected=%d", sum, expected)
-	t.Logf("CapacityAborts=%d", c.CapacityAborts())
-	if sum != expected {
-		t.Fatalf("Sum result is %d, but we expected %d", sum, expected)
-	}
-
-	runtime.GOMAXPROCS(oldmaxprocs)
+	t.Run("SystemMutex", func(t *testing.T) {
+		run(t, new(SystemMutex))
+	})
 }
