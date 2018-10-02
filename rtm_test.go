8b869acfb69a08ed3038a6289426feb6817912d2
Craig Hesling <craig@hesling.com>
2018-10-02 01:43:39 -0400

    Added a test for RTMContext

---
+++ rtm_test.go
@@ -0,0 +1,61 @@
+package safetyfast
+
+import (
+	"math/rand"
+	"runtime"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/intel-go/cpuid"
+)
+
+func TestRTMContext(t *testing.T) {
+	const numConcurGoRoutines = 8
+	const arrayLength = 100
+	const numIterations = 5000000
+
+	if !cpuid.HasExtendedFeature(cpuid.RTM) {
+		t.Fatal("The CPU does not support Intel RTM")
+	}
+
+	oldmaxprocs := runtime.GOMAXPROCS(numConcurGoRoutines)
+
+	var wg sync.WaitGroup
+	var arr = make([]int, arrayLength)
+	var c = NewRTMContexDefault()
+
+	routine := func() {
+		randsrc := rand.NewSource(int64(time.Now().Second()))
+		r := rand.New(randsrc)
+
+		for i := 0; i < numIterations; i++ {
+			index := r.Int() % len(arr)
+			c.Atomic(func() {
+				arr[index]++
+			})
+		}
+		wg.Done()
+	}
+
+	wg.Add(numConcurGoRoutines)
+	for i := 0; i < numConcurGoRoutines; i++ {
+		go routine()
+	}
+	wg.Wait()
+
+	var sum int
+	for _, v := range arr {
+		sum += v
+	}
+
+	expected := numIterations * numConcurGoRoutines
+	t.Logf("Array Length=%d | NumberGoRoutines=%d | NumIterations=%d", len(arr), numConcurGoRoutines, numIterations)
+	t.Logf("ArraySum=%d | Expected=%d", sum, expected)
+	t.Logf("CapacityAborts=%d", c.CapacityAborts())
+	if sum != expected {
+		t.Fatalf("Sum result is %d, but we expected %d", sum, expected)
+	}
+
+	runtime.GOMAXPROCS(oldmaxprocs)
+}
